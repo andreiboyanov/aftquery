caf18b8476f2d64fcee2c0242eaaeddde3750f9d
Andrei Boyanov <andrei.boyanov@gmail.com>
2018-08-13 15:09:05 +0200

    script to rertrieve AFT clubs and store them to the mongo db

---
+++ collector/collect_all_clubs.py
@@ -0,0 +1,26 @@
+import sys
+import pymongo
+from aftquery.parser.aft import clubs as aft_clubs
+
+
+def main(arguments):
+    client = pymongo.MongoClient()
+    db = client.aft_collector
+    for region in [1, 2, 3, 4]:
+        for club in aft_clubs.search_clubs(region=region):
+            club_id = club["id"]
+            db.clubs.find_and_modify(
+                query={"_id": club_id},
+                update={
+                    "$currentDate": {"last_updated": True},
+                    "$set": {
+                        "_id": club_id,
+                        "name": club["name"],
+                        "region": region,
+                    },
+                },
+                upsert=True,
+            )
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
