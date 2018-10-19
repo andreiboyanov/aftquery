94a5052cb268bc878831577c788dacdf40abc24e
Andrei Boyanov <andrei.boyanov@gmail.com>
2018-10-19 12:30:29 +0200

    Python 3,5 compatible tests

--- tests/api/rest/test_club.py
+++ tests/api/rest/test_club.py
@@ -4,6 +4,6 @@ import json
 def test_get_club_by_id(sousmarin):
     response = sousmarin.get("/club/1008")
     assert response.status_code == 200
-    club = json.loads(response.data)
+    club = json.loads(response.data.decode("utf-8"))
     assert club["_id"] == "1008"
     assert club["name"] == "BRUSSELS"
--- tests/api/rest/test_player.py
+++ tests/api/rest/test_player.py
@@ -4,6 +4,6 @@ import json
 def test_get_user_by_id(sousmarin):
     response = sousmarin.get("/player/1095130")
     assert response.status_code == 200
-    player = json.loads(response.data)
+    player = json.loads(response.data.decode("utf-8"))
     assert player["_id"] == "1095130"
     assert player["name"] == "BOYANOV Andrei"
