19fd39426359cef92a2ddfb1d8961cb0249b250c
Andrei Boyanov <andrei.boyanov@gmail.com>
2018-10-26 22:34:26 +0200

    Catch all exceptions

--- collector/collect_tournament_macthes.py
+++ collector/collect_tournament_macthes.py
@@ -1,6 +1,15 @@
 import sys
+from datetime import datetime
 import pymongo
 from aftquery.parser.aft import tournament as aft_tournament
+
+import traceback
+import logging
+
+logging.basicConfig(
+    filename="logs/{}_collect_tournament_matches.log".format(datetime.now()),
+    level=logging.DEBUG,
+)
 
 
 def add_tournament_matches(db, tournament_id):
@@ -17,15 +26,24 @@ def main(arguments):
 
     if len(arguments) > 0:
         add_tournament_matches(db, arguments[0])
-    all_tournaments = db.aft_tournaments.find()
+    all_tournaments = db.aft_tournaments.find({}, no_cursor_timeout=True)
     for index, tournament in enumerate(all_tournaments):
-        print(
-            "{}/{}".format(index, all_tournaments.count()),
-            tournament["_id"],
-            tournament["name"],
-            tournament["date"],
-        )
-        add_tournament_matches(db, tournament["_id"])
+        try:
+            print(
+                "{}/{}".format(index, all_tournaments.count()),
+                tournament["_id"],
+                tournament["name"],
+                tournament["date"],
+            )
+            add_tournament_matches(db, tournament["_id"])
+        except Exception as error:
+            logging.error(
+                "{} {} {} ==> {}".format(
+                    tournament["_id"], tournament["name"], tournament["date"], error
+                )
+            )
+            logging.debug(traceback.format_exc())
+    all_tournaments.close()
 
 
 if __name__ == "__main__":
