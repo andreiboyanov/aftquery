6545998e8eee9e573cb229acc4f4abf48342f5e5
andreiboyanov <andrei.boyanov@gmail.com>
2018-10-31 07:41:09 +0100

    Print curent player index and the total number of players when puling player detauls

--- collector/update_player_details.py
+++ collector/update_player_details.py
@@ -6,9 +6,11 @@ from aftquery.parser.aft import players as aft_players
 def main(arguments):
     client = pymongo.MongoClient()
     db = client.aft_collector
-    for player in db.players.find({'details': {'$exists': False}}):
+    all_players = db.players.find({'details': {'$exists': False}}, no_cursor_timeout=True)
+    count = all_players.count()
+    for index, player in enumerate(all_players):
         player_id = player["_id"]
-        print(player_id, player["name"])
+        print("{}/{}".format(index, count), player_id, player["name"])
         player_details = aft_players.get_player_details(player_id=player_id)
         db.players.find_and_modify(
             query={"_id": player_id},
@@ -20,6 +22,7 @@ def main(arguments):
             },
             upsert=False,
         )
+    all_players.close()
 
 
 if __name__ == "__main__":
