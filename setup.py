054f7ed621909c6fe324ce1515cdebb8528597d5
saadmanrafat <saadman@outlook.com>
2018-12-22 16:31:25 +0600

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+NAME = "aftquery"
+DESCRIPTION = (
+    "A command line tools for discovering info about"
+    "tennis tournaments and players in Belgium"
+)
+URL = "https://github.com/andreiboyanov/aftquery"
+EMAIL = "andrei.boyanov@gmail.com"
+
+AUTHOR = "Andrei Boyanov"
+
+setup(
+    name=NAME,
+    version="0.1.0",
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    install_requires=["pymongo", "Flask-PyMongo", "beautifulsoup4", "requests"],
+    packages=find_packages(exclude=("tests",)),
+    python_requires=">=3.0",
+    include_package_data=True,
+    license="GPL",
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
