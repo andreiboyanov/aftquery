e6e23bb22eb6c7ee3c4480b99422c0ef82e84b12
Andrei Boyanov <andrei.boyanov@gmail.com>
2019-05-08 11:50:51 +0200

    Chage the API to use flask-restplus
    A really cool API like that

--- aftquery/api/rest/sousmarin.py
+++ aftquery/api/rest/sousmarin.py
@@ -1,26 +1,29 @@
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
+from flask_restplus import Resource, Api
 
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/aft_collector"
 mongo = PyMongo(app)
+api = Api(app)
 
 
-@app.route("/player/<player_id>", methods=['GET'])
-def get_player(player_id):
-    players = mongo.db.players
-    player = players.find_one({'_id': player_id})
-    return jsonify(player)
+@api.route("/player/<string:player_id>")
+class Player(Resource):
+    def get(self, player_id):
+        players = mongo.db.players
+        player = players.find_one({'_id': player_id})
+        return jsonify(player)
 
 
-@app.route("/club/<club_id>", methods=['GET'])
-def get_club(club_id):
-    clubs = mongo.db.clubs
-    club = clubs.find_one({'_id': club_id})
-    return jsonify(club)
+@api.route("/club/<string:club_id>")
+class Club(Resource):
+    def get(self, club_id):
+        clubs = mongo.db.clubs
+        club = clubs.find_one({'_id': club_id})
+        return jsonify(club)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7777)
-
+    app.run(host="0.0.0.0", port=7777, debug=True)
