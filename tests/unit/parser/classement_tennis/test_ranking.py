e1917ac877d90a70672075e7fcd2ea8b037bf008
Andrei Boyanov <andrei.boyanov@gmail.com>
2019-06-12 19:27:06 +0200

    ÂRemove classement-tennis form the tests

--- tests/unit/parser/classement_tennis/test_ranking.py
+++
@@ -1,7 +0,0 @@
-from aftquery.parser.classement_tennis.ranking import get_ranking_prevision
-
-
-def test_get_ranking_prevision():
-    player_id = "1095130"
-    new_ranking = get_ranking_prevision(player_id)
-    assert new_ranking == "C30.2"
