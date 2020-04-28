3f17d594c28bf69efc7ba83f8d4c839d2f1123a5
Andrei Boyanov <andrei.boyanov@gmail.com>
2020-04-28 08:45:28 +0200

    Fix typo

--- tests/integration/test_aftsearch.py
+++ tests/integration/test_aftsearch.py
@@ -6,6 +6,6 @@ THIS_DIR = os.path.dirname(__file__)
 AFTQUERY_PATH = os.path.join(THIS_DIR, os.pardir, os.pardir, "aftquery")
 
 
-def test_afrsearch_help():
+def test_aftsearch_help():
     return_code = subprocess.call(["python", os.path.join(AFTQUERY_PATH, "aftsearch.py"), "--help"])
     assert return_code == 0
